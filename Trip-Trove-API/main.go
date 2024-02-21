9213d9f90d525b6977b4dabbee68c969e7e29ee0
Oana-Andreea Ilie <andreeaa.ilie516@gmail.com>
2024-02-21 12:39:41 +0200

    User CRUD operations added

--- Trip-Trove-API/main.go
+++ Trip-Trove-API/main.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"Trip-Trove-API/database"
+	"Trip-Trove-API/domain/entities"
+	"Trip-Trove-API/domain/services"
+	"Trip-Trove-API/infrastructure/dataaccess"
+	"Trip-Trove-API/infrastructure/middlewares"
+	"Trip-Trove-API/presentation/handlers"
+	"Trip-Trove-API/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"log"
@@ -14,7 +20,28 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	_ = database.ConnectDB()
+	db := database.ConnectDB()
+
+	entitiesToMigrate := []interface{}{
+		&entities.User{},
+	}
+
+	for _, entity := range entitiesToMigrate {
+		err := db.AutoMigrate(entity)
+		if err != nil {
+			log.Fatalf("Failed to migrate database: %v", err)
+		}
+	}
+
+	authMiddleware := middlewares.AuthMiddleware{}
+
+	userRepository := dataaccess.NewGormUserRepository(db)
+
+	userService := services.UserService{Repo: userRepository}
+
+	userHandler := handlers.UserHandler{Service: &userService}
+
+	routes.RegisterUserRoutes(router, &userHandler, authMiddleware)
 
 	err := router.Run("localhost:8080")
 	if err != nil {
